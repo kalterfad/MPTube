501ec36b4b331c8b62d8746568ebba6627ad33cc
kalterfad <f.tiratore.k@gmail.com>
2023-05-26 13:30:14 +0500

    v2

---
+++ README.md
@@ -0,0 +1,23 @@
+```shell
+go build -o mptube main.go
+```
+
+```shell
+sudo mv mptube /usr/local/bin/
+```
+
+```shell
+sudo apt install libnotify-bin
+```
+
+```shell
+sudo cp mptube.desktop /usr/share/applications/
+```
+
+```shell
+sudo mkdir /usr/share/icons/MPTube/
+```
+
+```shell
+sudo cp icon.svg /usr/share/icons/MPTube/
+```
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -19,28 +20,44 @@ type VideoLink struct {
 }
 
 func main() {
+	err := notification("Start MPTube")
+	if err != nil {
+		log.Fatal(err)
+	}
 	http.HandleFunc("/link", openMPV)
 	log.Fatal(http.ListenAndServe(":9086", nil))
+}
+
+func notification(message string) error {
+	cmd := exec.Command(
+		"notify-send",
+		"-i", "/usr/share/icons/MPTube/icon.svg",
+		"MPTube", message)
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("notfication err:%v", err)
+	}
+	return nil
 }
 
 func openMPV(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	var videoLink VideoLink
 	err := decoder.Decode(&videoLink)
-
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	_ = notification("Start video")
 	cmd := exec.Command("mpv", videoLink.Url)
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
-	log.Printf("start video: '%v'", videoLink.Url)
 	err = cmd.Run()
-	log.Printf("end video: '%v'", videoLink.Url)
+
 	if err != nil {
-		log.Printf("cmd.Run err: '%v'", err)
+		_ = notification("Open error: \n" + err.Error())
+		return
 	}
 }
